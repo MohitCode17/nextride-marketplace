38488d7b51ec39c4c7859956bd19fbd15887d5d8
Mohit Gupta <mohit.codes17@gmail.com>
2025-03-20 15:50:57 +0530

    add clerk auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,16 +13,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header />
-        <main className="min-h-screen">{children}</main>
-        <footer className="bg-blue-100 py-12">
-          <div className="container mx-auto text-center text-gray-700 px-4">
-            <p>Made with ❤️ by MohitCodes</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-blue-100 py-12">
+            <div className="container mx-auto text-center text-gray-700 px-4">
+              <p>Made with ❤️ by MohitCodes</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,25 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      {/* IF SIGOUT THEN DISPLAY SIGNIN AND SIGNUP BUTTON */}
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      {/* IF SIGNIN THEN DISPLAY USER BUTTON */}
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Header;
