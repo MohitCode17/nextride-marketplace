2bae60378898dc01aab076b978a76aa140fed613
MohitCode17 <mohit.codes17@gmail.com>
2025-03-23 12:05:05 +0530

    add auth layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,5 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 const Header = () => {
-  return (
-    <div>
-      {/* IF SIGOUT THEN DISPLAY SIGNIN AND SIGNUP BUTTON */}
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      {/* IF SIGNIN THEN DISPLAY USER BUTTON */}
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
